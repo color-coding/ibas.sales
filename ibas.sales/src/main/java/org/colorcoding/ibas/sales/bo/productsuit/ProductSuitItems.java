53c92b044ff1f0d5911f2a908bb3d5fada50d764
Aton5859 <390072015@qq.com>
2017-09-22 15:10:45 +0800

    合并

--- ibas.sales/src/main/java/org/colorcoding/ibas/sales/bo/productsuit/ProductSuitItems.java
+++ ibas.sales/src/main/java/org/colorcoding/ibas/sales/bo/productsuit/ProductSuitItems.java
@@ -4,12 +4,12 @@ import java.beans.PropertyChangeEvent;
 import javax.xml.bind.annotation.*;
 import org.colorcoding.ibas.bobas.common.*;
 import org.colorcoding.ibas.bobas.bo.*;
-import org.colorcoding.ibas.sales.MyConsts;
+import org.colorcoding.ibas.sales.MyConfiguration;
 
 /**
 * 产品套装-项目 集合
 */
-@XmlType(name = ProductSuitItems.BUSINESS_OBJECT_NAME, namespace = MyConsts.NAMESPACE_BO)
+@XmlType(name = ProductSuitItems.BUSINESS_OBJECT_NAME, namespace = MyConfiguration.NAMESPACE_BO)
 @XmlSeeAlso({ ProductSuitItem.class })
 public class ProductSuitItems extends BusinessObjects<IProductSuitItem, IProductSuit> implements IProductSuitItems {
 
--- ibas.sales/src/main/java/org/colorcoding/ibas/sales/bo/salesorder/SalesOrderItems.java
+++ ibas.sales/src/main/java/org/colorcoding/ibas/sales/bo/salesorder/SalesOrderItems.java
@@ -4,12 +4,12 @@ import java.beans.PropertyChangeEvent;
 import javax.xml.bind.annotation.*;
 import org.colorcoding.ibas.bobas.common.*;
 import org.colorcoding.ibas.bobas.bo.*;
-import org.colorcoding.ibas.sales.MyConsts;
+import org.colorcoding.ibas.sales.MyConfiguration;
 
 /**
 * 销售订单-行 集合
 */
-@XmlType(name = SalesOrderItems.BUSINESS_OBJECT_NAME, namespace = MyConsts.NAMESPACE_BO)
+@XmlType(name = SalesOrderItems.BUSINESS_OBJECT_NAME, namespace = MyConfiguration.NAMESPACE_BO)
 @XmlSeeAlso({ SalesOrderItem.class })
 public class SalesOrderItems extends BusinessObjects<ISalesOrderItem, ISalesOrder> implements ISalesOrderItems {
 
