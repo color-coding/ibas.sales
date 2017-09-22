f75103f3b079871c7c80d9a65b30fe6ceb3a17a2
GSGSS <342517845@QQ.COM>
2017-09-22 15:52:26 +0800

    修复java端不正常修改

--- ibas.sales/src/main/java/org/colorcoding/ibas/sales/MyConfiguration.java
+++ ibas.sales/src/main/java/org/colorcoding/ibas/sales/MyConfiguration.java
@@ -4,6 +4,29 @@ package org.colorcoding.ibas.sales;
  * 我的配置项
  */
 public class MyConfiguration extends org.colorcoding.ibas.bobas.MyConfiguration {
+	/**
+	 * 模块标识
+	 */
+	public static final String MODULE_ID = "bc24810f-da83-4e99-9252-d22bc28b614c";
 
+	/**
+	 * 命名空间
+	 */
+	public static final String NAMESPACE_ROOT = "http://colorcoding.org/ibas/sales/";
+
+	/**
+	 * 数据命名空间
+	 */
+	public static final String NAMESPACE_DATA = NAMESPACE_ROOT + "data";
+
+	/**
+	 * 业务对象命名空间
+	 */
+	public static final String NAMESPACE_BO = NAMESPACE_ROOT + "bo";
+
+	/**
+	 * 服务命名空间
+	 */
+	public static final String NAMESPACE_SERVICE = NAMESPACE_ROOT + "service";
 
 }
