c449d352db741a5a32983347a47ecc38c5d0a248
niurenzhu <niuren.zhu@icloud.com>
2018-06-27 23:39:41 +0800

    更新配置机制

--- ibas.sales.service/src/main/java/org/colorcoding/ibas/sales/service/rest/FileService.java
+++ ibas.sales.service/src/main/java/org/colorcoding/ibas/sales/service/rest/FileService.java
@@ -28,8 +28,7 @@ import org.glassfish.jersey.media.multipart.FormDataMultiPart;
 @Path("file")
 public class FileService extends FileRepositoryService {
 
-	public final static String WORK_FOLDER = MyConfiguration.getConfigValue(
-			MyConfiguration.CONFIG_ITEM_SALES_FILE_FOLDER,
+	public final static String WORK_FOLDER = MyConfiguration.getConfigValue(MyConfiguration.CONFIG_ITEM_DOCUMENT_FOLDER,
 			MyConfiguration.getDataFolder() + File.separator + "sales_files");
 
 	public FileService() {
--- ibas.sales/src/main/java/org/colorcoding/ibas/sales/MyConfiguration.java
+++ ibas.sales/src/main/java/org/colorcoding/ibas/sales/MyConfiguration.java
@@ -1,9 +1,36 @@
 package org.colorcoding.ibas.sales;
 
+import org.colorcoding.ibas.bobas.configuration.ConfigurationFactory;
+import org.colorcoding.ibas.bobas.configuration.IConfigurationManager;
+
 /**
  * 我的配置项
  */
 public class MyConfiguration extends org.colorcoding.ibas.bobas.MyConfiguration {
+
+	private volatile static IConfigurationManager instance;
+
+	public static IConfigurationManager create() {
+		if (instance == null) {
+			synchronized (MyConfiguration.class) {
+				if (instance == null) {
+					instance = ConfigurationFactory.create().createManager();
+					instance.setConfigSign(MODULE_ID);
+					instance.update();
+				}
+			}
+		}
+		return instance;
+	}
+
+	public static <P> P getConfigValue(String key, P defaultValue) {
+		return create().getConfigValue(key, defaultValue);
+	}
+
+	public static String getConfigValue(String key) {
+		return create().getConfigValue(key);
+	}
+
 	/**
 	 * 模块标识
 	 */
@@ -28,9 +55,5 @@ public class MyConfiguration extends org.colorcoding.ibas.bobas.MyConfiguration 
 	 * 服务命名空间
 	 */
 	public static final String NAMESPACE_SERVICE = NAMESPACE_ROOT + "service";
-	/**
-	 * 配置项目-文件文件夹
-	 */
-	public final static String CONFIG_ITEM_SALES_FILE_FOLDER = "SLFileFolder";
 
 }
