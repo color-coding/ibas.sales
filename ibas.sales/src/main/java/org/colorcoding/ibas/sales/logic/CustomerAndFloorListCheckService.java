ed73bc7b9e64c0c09dc535340c34c1ac14714e10
牛加人等于朱 <niuren.zhu@icloud.com>
2022-11-07 17:22:35 +0800

    调整低价比较逻辑

--- ibas.sales/src/main/java/org/colorcoding/ibas/sales/logic/CustomerAndFloorListCheckService.java
+++ ibas.sales/src/main/java/org/colorcoding/ibas/sales/logic/CustomerAndFloorListCheckService.java
@@ -1,9 +1,11 @@
 package org.colorcoding.ibas.sales.logic;
 
+import org.colorcoding.ibas.bobas.approval.IApprovalData;
 import org.colorcoding.ibas.bobas.common.ConditionOperation;
 import org.colorcoding.ibas.bobas.common.Criteria;
 import org.colorcoding.ibas.bobas.common.ICondition;
 import org.colorcoding.ibas.bobas.common.ICriteria;
+import org.colorcoding.ibas.bobas.data.emApprovalStatus;
 import org.colorcoding.ibas.bobas.mapping.LogicContract;
 import org.colorcoding.ibas.materials.bo.materialpricelist.IMaterialPriceList;
 import org.colorcoding.ibas.materials.bo.materialpricelist.MaterialPriceList;
@@ -17,6 +19,18 @@ import org.colorcoding.ibas.materials.repository.BORepositoryMaterials;
  */
 @LogicContract(ICustomerAndFloorListCheckContract.class)
 public class CustomerAndFloorListCheckService extends org.colorcoding.ibas.businesspartner.logic.CustomerCheckService {
+
+	@Override
+	protected boolean checkDataStatus(Object data) {
+		// 审批中也执行
+		if (data instanceof IApprovalData) {
+			if (((IApprovalData) data).getApprovalStatus() == emApprovalStatus.PROCESSING) {
+				return true;
+			}
+		}
+		return super.checkDataStatus(data);
+	}
+
 	@Override
 	protected void impact(org.colorcoding.ibas.businesspartner.logic.ICustomerCheckContract contract) {
 		super.impact(contract);
