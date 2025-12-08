a51a103c59676e32bbb4004fc275f23108252109
Niuren.Zhu <niuren.zhu@icloud.com>
2025-12-08 12:36:28 +0800

    修复开启成本计算取消逻辑问题

--- ibas.sales/src/main/java/org/colorcoding/ibas/sales/bo/salescreditnote/SalesCreditNoteItem.java
+++ ibas.sales/src/main/java/org/colorcoding/ibas/sales/bo/salescreditnote/SalesCreditNoteItem.java
@@ -10,6 +10,7 @@ import javax.xml.bind.annotation.XmlType;
 
 import org.colorcoding.ibas.accounting.logic.IJECPropertyValueGetter;
 import org.colorcoding.ibas.accounting.logic.ITaxGroupCheckContract;
+import org.colorcoding.ibas.bobas.approval.IApprovalData;
 import org.colorcoding.ibas.bobas.bo.BusinessObject;
 import org.colorcoding.ibas.bobas.bo.IBOTagCanceled;
 import org.colorcoding.ibas.bobas.bo.IBOTagDeleted;
@@ -18,6 +19,7 @@ import org.colorcoding.ibas.bobas.core.IPropertyInfo;
 import org.colorcoding.ibas.bobas.data.ArrayList;
 import org.colorcoding.ibas.bobas.data.DateTime;
 import org.colorcoding.ibas.bobas.data.Decimal;
+import org.colorcoding.ibas.bobas.data.emApprovalStatus;
 import org.colorcoding.ibas.bobas.data.emBOStatus;
 import org.colorcoding.ibas.bobas.data.emDocumentStatus;
 import org.colorcoding.ibas.bobas.data.emYesNo;
@@ -3034,6 +3036,13 @@ public class SalesCreditNoteItem extends BusinessObject<SalesCreditNoteItem> imp
 					if (SalesCreditNoteItem.this.parent instanceof IBOTagDeleted) {
 						IBOTagDeleted boTag = (IBOTagDeleted) SalesCreditNoteItem.this.parent;
 						if (boTag.getDeleted() == emYesNo.YES) {
+							return true;
+						}
+					}
+					if (SalesCreditNoteItem.this.parent instanceof IApprovalData) {
+						IApprovalData apData = (IApprovalData) SalesCreditNoteItem.this.parent;
+						if (apData.getApprovalStatus() != emApprovalStatus.UNAFFECTED
+								&& apData.getApprovalStatus() != emApprovalStatus.APPROVED) {
 							return true;
 						}
 					}
