b19be2e906229f713d7f6b8fbe4ddfa05d244b86
Niuren.Zhu <niuren.zhu@icloud.com>
2026-09-10 12:11:17 +0800

    Monthly merge: master -> v0.2.x (2026-09.1)

--- ibas.sales/src/main/java/org/colorcoding/ibas/sales/rules/BusinessRuleDeductionDocumentTotal.java
+++ ibas.sales/src/main/java/org/colorcoding/ibas/sales/rules/BusinessRuleDeductionDocumentTotal.java
@@ -4,13 +4,11 @@ import java.math.BigDecimal;
 
 import org.colorcoding.ibas.bobas.core.IPropertyInfo;
 import org.colorcoding.ibas.bobas.common.Decimals;
-import org.colorcoding.ibas.bobas.i18n.I18N;
 import org.colorcoding.ibas.bobas.rule.BusinessRuleCommon;
 
 public class BusinessRuleDeductionDocumentTotal extends BusinessRuleCommon {
 
 	protected BusinessRuleDeductionDocumentTotal() {
-		this.setName(I18N.prop("msg_sl_business_rule_deduction_document_total"));
 	}
 
 	/**
